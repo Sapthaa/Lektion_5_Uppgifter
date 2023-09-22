b6b60c8d1ffd39a74e851745860c56a5c6f1a852
sapthaa <andre.saptha@hotmail.com>
2023-09-22 03:27:30 +0200

    Uppgift 2

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Uppgift 2
+        System.out.println("Skriv ett tal: ");
+        Scanner tal = new Scanner(System.in);
 
-        System.out.println("Welcome, enter your name: ");
-        Scanner scanner = new Scanner(System.in);
+        int x = tal.nextInt();
+        for(int i = 0; i < x; i++){
+            System.out.println("Loopet kommer köras " + x + " gånger");
+        }
 
-        System.out.println("Welcome player #1: " + scanner.nextLine());
 
 
 
