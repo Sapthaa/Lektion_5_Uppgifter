382d2ed92b6edc8b8d3543797f53b7c24aff7fda
sapthaa <andre.saptha@hotmail.com>
2023-09-22 03:19:51 +0200

    Uppgift 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome, enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome player #1: " + scanner.nextLine());
+
+
+
+
+    }
+}
